6eb3390b875f7019f1661650c60a8bbd43f950de
unaya11 <191686788+unaya11@users.noreply.github.com>
2026-01-20 20:36:23 +0900

    feature/add-pagination (#8)
    * feat: add replyPage pagination
    
    * feat: add replyPage pagination
    
    * feat: add topPage pagination
    
    ---------
    
    Co-authored-by: unaya11 <kazuki.tanaka@fox-hound.tech>

--- src/main/kotlin/com/bulletinboard/controller/BulletinBoardController.kt
+++ src/main/kotlin/com/bulletinboard/controller/BulletinBoardController.kt
@@ -3,6 +3,7 @@ package com.bulletinboard.controller
 import com.bulletinboard.service.MessageService
 import com.bulletinboard.service.ReplyService
 import com.bulletinboard.service.UserService
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -19,8 +20,9 @@ class BulletinBoardController(
     private val replyService: ReplyService,
 ) {
     @GetMapping("top")
-    fun viewTop(model: Model): String {
-        model.addAttribute("message", messageService.findAll())
+    fun viewTop(model: Model, pageable: Pageable,): String {
+        val message = messageService.findAll(pageable)
+        model.addAttribute("message", message)
         return "top"
     }
 
@@ -39,8 +41,11 @@ class BulletinBoardController(
     fun replyPage(
         @PathVariable messageId: Int,
         model: Model,
+        pageable: Pageable,
     ): String {
-        model.addAttribute("message", messageService.findByParentMessage(messageId))
+        val replyMessage = replyService.findByReplyMessage(pageable, messageId)
+        model.addAttribute("parentMessage", messageService.findByParentMessage(messageId))
+        model.addAttribute("replyMessage", replyMessage)
         return "reply"
     }
 
