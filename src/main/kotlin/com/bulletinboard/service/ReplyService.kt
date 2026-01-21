5e78414fca91ee3f80c66a34909437d9fc99dbdb
unaya11 <kazuki.tanaka@fox-hound.tech>
2026-01-21 09:15:06 +0900

    fix: display order

--- src/main/kotlin/com/bulletinboard/service/ReplyService.kt
+++ src/main/kotlin/com/bulletinboard/service/ReplyService.kt
@@ -5,6 +5,7 @@ import com.bulletinboard.repository.MessageRepository
 import com.bulletinboard.repository.ReplyRepository
 import com.bulletinboard.repository.UserRepository
 import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
@@ -17,7 +18,16 @@ class ReplyService(
     fun findByReplyMessage(
         pageable: Pageable,
         messageId: Int,
-    ): Page<Reply> = replyRepository.findByReplyMessage(pageable, messageId)
+    ): Page<Reply> {
+        val replyMessage = replyRepository.findByReplyMessage(pageable, messageId)
+        return reverseContent(replyMessage)
+    }
+
+    fun reverseContent(originalPage: Page<Reply>): Page<Reply> {
+        val reverseReply = originalPage.content.asReversed()
+        val reversedList = PageImpl(reverseReply, originalPage.pageable, originalPage.totalElements)
+        return reversedList
+    }
 
     fun replySave(
         userId: Int,
