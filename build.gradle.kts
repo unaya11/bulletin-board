e1abf03199f33b6bd90d67ae562d84d23073fb97
unaya11 <kazuki.tanaka@fox-hound.tech>
2026-01-24 14:17:00 +0900

    feat: update springboot and kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "2.2.21"
-    kotlin("plugin.spring") version "2.2.21"
-    id("org.springframework.boot") version "4.0.1"
+    kotlin("jvm") version "2.3.0"
+    kotlin("plugin.spring") version "2.3.0"
+    id("org.springframework.boot") version "4.0.2"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
     // Kotlinでjpaを扱う場合、初期値が無いと実行時エラーとなる。初期値を入れるか以下を追加する。
