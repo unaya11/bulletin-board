4456d74f21311bb636d652a520ec5a24faf2a80d
unaya11 <aaa@example.com>
2026-02-10 09:33:28 +0900

    feat: add Config.kt

---
+++ src/main/kotlin/com/bulletinboard/config/Config.kt
@@ -0,0 +1,8 @@
+package com.bulletinboard.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class Config
